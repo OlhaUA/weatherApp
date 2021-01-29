056202c08b37dd99d6d1d57f17f3f1e73b3b4fee
OlhaUA <olhaanhelua@gmail.com>
2021-01-29 21:36:31 +0200

    was https problem

--- weather.js
+++ weather.js
@@ -8,7 +8,7 @@ class Weather {
   // Fetch weather from API
   async getWeather() {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.code}&units=metric&appid=${this.apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.code}&units=metric&appid=${this.apiKey}`
     );
 
     const responseData = await response.json();
